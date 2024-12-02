cd89edca8da7a8f812556a628409408d8c0ba369
João Pedro <147161269+Joao2007Pedro@users.noreply.github.com>
2024-12-02 08:55:38 -0300

    index.js
    Implementa sistema de upload de imagens e cadastro de candidatos com Express, MySQL e Handlebars. Adiciona Bootstrap para estilização, configuração de rotas para cadastro, e teste de conexão com o banco de dados. Manipulação de arquivos habilitada com express-fileupload

--- Projeto Eleição/index.js
+++ Projeto Eleição/index.js
@@ -22,14 +22,22 @@ app.use(fileupload());
 // Adicionar Bootstrap
 app.use('/bootstrap', express.static('./node_modules/bootstrap/dist'));
 
-// Adicionar css
+// Adicionar CSS
 app.use('/css', express.static('./css'))
+
 
 // Referenciar a pasta de imagens
 app.use('/imagens', express.static('./imagens'));
 
 // Configuração do express-handlebars
-app.engine('handlebars', engine());
+app.engine('handlebars', engine({
+    defaultLayout: 'main', // Definindo o layout principal
+    helpers: {
+        condicionalIgualdade: function (parametro1, parametro2, options) {
+            return parametro1 === parametro2 ? options.fn(this) : options.inverse(this);
+        }
+    }
+}));
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
@@ -53,58 +61,140 @@ conexao.connect(function(erro) {
 });
 
 // Rota principal
-app.get('/', function(req, res){
+app.get('/index', function(req, res){
     // SQL
     let sql = 'SELECT * FROM candidatos';
 
     // Executar comando SQL
     conexao.query(sql, function(erro, retorno){
-        res.render('formulario', {candidatos:retorno});
+        res.render('index', {candidatos:retorno});
+    });
+});
+
+// Rota de Votação
+app.get('/voto', function (req, res) {
+    let sql = 'SELECT * FROM candidatos';
+
+    conexao.query(sql, function (erro, candidatos) {
+        if (erro) {
+            console.error('Erro ao buscar candidatos:', erro);
+            res.status(500).send('Erro ao carregar a página de votação.');
+            return;
+        }
+        // Enviar dados para o template
+        res.render('voto', { candidatos: candidatos });
+    });
+});
+
+app.get('/votar/:idCandidato', function (req, res) {
+    const idCandidato = req.params.idCandidato;
+
+    // SQL para atualizar o número de votos
+    let sql = `UPDATE candidatos SET votos = votos + 1 WHERE idCandidato = ${idCandidato}`;
+
+    conexao.query(sql, function (erro, retorno) {
+        if (erro) {
+            console.error('Erro ao registrar o voto:', erro);
+            res.status(500).send('Erro ao registrar o voto.');
+            return;
+        }
+
+        // Redirecionar para uma página de sucesso
+        res.redirect('/votoRegistrado');
+    });
+});
+
+// Página de sucesso
+app.get('/votoRegistrado', function (req, res) {
+    res.render('votoRegistrado');
+});
+
+app.get('/resultado', function (req, res) {
+    // SQL para buscar candidatos e votos
+    let sql = 'SELECT nome, votos FROM candidatos ORDER BY votos DESC';
+
+    conexao.query(sql, function (erro, candidatos) {
+        if (erro) {
+            console.error('Erro ao carregar resultados:', erro);
+            res.status(500).send('Erro ao carregar resultados.');
+            return;
+        }
+
+        // Renderizar a página com os resultados
+        res.render('resultado', { candidatos: candidatos });
+    });
+});
+
+
+// Rota principal contendo a situação
+app.get('/:situacao', function(req, res){
+    // SQL
+    let sql = 'SELECT * FROM candidatos';
+
+    // Executar comando SQL
+    conexao.query(sql, function(erro, retorno){
+        res.render('formulario', {candidatos:retorno, situacao:req.params.situacao});
     });
 });
 
 // Rota de Cadastro 
 app.post('/cadastrar', function(req, res) {
-    // Obter os dados que serão utilizados para o cadastro
-    let nome = req.body.nome;
-    let numero = req.body.numero;
-    let imagem = req.files.imagem.name;
+    try {
+        // Obter os dados que serão utilizados para o cadastro
+        let nome = req.body.nome;
+        let numero = req.body.numero;
+        let imagem = req.files.imagem.name;
 
-    //SQL
-    let sql = `INSERT INTO candidatos (nome, numero, imagem) VALUES ('${nome}', ${numero}, '${imagem}')`;
+        // Validar o nome do candidato e numero
+        if (nome == ''|| numero == '' || isNaN(numero)) {
+            res.redirect('/falhaCadastro');
+        }else {
+            //SQL
+            let sql = `INSERT INTO candidatos (nome, numero, imagem) VALUES ('${nome}', ${numero}, '${imagem}')`;
 
-    // Executar comando SQL
-    conexao.query(sql, function(erro, retorno){
-        // Caso ocorra algum erro
-        if (erro) throw erro;
+            // Executar comando SQL
+            conexao.query(sql, function(erro, retorno){
+                // Caso ocorra algum erro
+                if (erro) throw erro;
 
-        // Caso ocorra o cadastro
-        req.files.imagem.mv(__dirname+'/imagens/'+req.files.imagem.name); 
-        console.log(retorno);
-    });
+                // Caso ocorra o cadastro
+                req.files.imagem.mv(__dirname+'/imagens/'+req.files.imagem.name); 
+                console.log(retorno);
+            });
 
-    // Retornar para a rota principal
-    res.redirect('/');
+            // Retornar para a rota principal
+            res.redirect('/okCadastro');
+        }
+    }catch(erro){
+        res.redirect('/falhaCadastro');
+    }
 });
 
 // Rota para remover produtos
 app.get('/remover/:idCandidato&:imagem', function(req, res){
-    //SQL
-    let sql = `DELETE FROM candidatos WHERE idCandidato = ${req.params.idCandidato}`;
+    
+    // Tratamento de exceção 
+    try {
+        //SQL
+        let sql = `DELETE FROM candidatos WHERE idCandidato = ${req.params.idCandidato}`;
 
-    // Executar o comando SQL
-    conexao.query(sql, function(erro, retorno){
-        // Caso falhe o comando SQL
-        if(erro) throw erro;
+        // Executar o comando SQL
+        conexao.query(sql, function(erro, retorno){
+            // Caso falhe o comando SQL
+            if(erro) throw erro;
 
-        //Caso o comando SQL funcione
-        fs.unlink(__dirname+'/imagem/'+req.params.imagem, (erro_imagem)=>{
-            console.log('Falha ao remover a imagem');
+            //Caso o comando SQL funcione
+            fs.unlink(__dirname+'/imagem/'+req.params.imagem, (erro_imagem)=>{
+                console.log('Falha ao remover a imagem');
+            });
         });
-    });
 
-    // Redirecionamento
-    res.redirect('/');
+        // Redirecionamento
+        res.redirect('/okRemover');
+    }catch(erro){
+        res.redirect('/falhaRemover');
+    }
+
 });
 
 // Rota para redirecionar para o formulario de alteração/edição
@@ -124,9 +214,58 @@ app.get('/formularioEditar/:idCandidato', function(req, res){
 });
 
 // Rota para editar Candidatos
-app.post('./editar', function(req, res){
+    app.post('/editar', function(req, res){
     
-})
+
+    // Obter os dados do formulario 
+    let nome = req.body.nome;
+    let numero = req.body.numero;
+    let idCandidato = req.body.idCandidato;
+    let nomeImagem = req.body.nomeImagem;
+
+    // Validar nome do candidato e numero
+    if (nome == '' || numero == '' || isNaN(numero)) {
+        res.redirect('/falhaEdicao');
+    } else {
+
+        // Definir o tipo de edição
+        try{
+            // Objeto de imagem
+            let imagem = req.files.imagem;
+
+            // SQL
+            let sql = `UPDATE candidatos SET nome='${nome}', numero=${numero}, imagem='${imagem.name}' WHERE idCandidato=${idCandidato}`;
+
+            // Executar comando SQL
+            conexao.query(sql, function(erro, retorno){
+                // Caso falhe o comando SQL
+                if(erro) throw erro;
+
+                // Remover imagem antiga
+                fs.unlink(__dirname+'/imagens/'+nomeImagem, (erro_imagem)=>{
+                console.log('Falha ao remover a imagem.');
+            });
+
+                // Cadastrar nova imagem
+                imagem.mv(__dirname+'/imagens/'+imagem.name);
+        });
+    }catch(erro){
+        
+        // SQL
+        let sql = `UPDATE candidatos SET nome='${nome}', numero=${numero} WHERE idCandidato=${idCandidato}`;
+
+        // Executar comando SQL
+        conexao.query(sql, function(erro, retorno){
+            // Caso falhe o comando SQL
+            if(erro) throw erro;
+        });
+    }
+
+
+        // Redirecionamento
+        res.redirect('/okEdicao')
+    }
+});
 
 //Servidor 
-app.listen(8080);
+app.listen(3030);
